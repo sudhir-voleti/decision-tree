340b187d67054169da03b2ed66310545b81dfbe7
Shivani Srivastava <shivani141296@gmail.com>
2023-12-06 10:34:21 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -117,7 +117,7 @@ shinyUI(
                              h4('Validation Result Summary'),
                              #verbatimTextOutput("validation"),
                              verbatimTextOutput("validation1"),
-                             tableOutput("validation"),
+                             tableOutput("validation0"),
                              plotOutput('plot_pred')
                              # h4('Detailed summary of splits'),
                              # verbatimTextOutput("summary")
