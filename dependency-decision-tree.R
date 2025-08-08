7fac2aa87d28a8877df7f2a31dd8dbce39b37fa8
Sudhir Voleti <sudhir_voleti@isb.edu>
2025-08-08 14:46:33 +0530

    Update dependency-decision-tree.R

--- dependency-decision-tree.R
+++ dependency-decision-tree.R
@@ -12,12 +12,10 @@ suppressPackageStartupMessages({
   if (!require('hydroTSM')){install_github("hzambran/hydroTSM")}; library('hydroTSM') 
   if (!require('hydroGOF')){install_github("hzambran/hydroGOF")}; library('hydroGOF')
   
-  #if (!require('hydroGOF')){install.packages('hydroGOF')}; library(hydroGOF)
   if (!require('sparkline')){install.packages('sparkline')}; library(sparkline)
   if (!require('partykit')){install.packages('partykit')}; library(partykit)
   if (!require('visNetwork')){install.packages('visNetwork')};library(visNetwork)
   if (!require('DT')){install.packages('DT')};library(DT)
   if (!require('tidyr')){install.packages('tidyr')};library(tidyr)
-  #install_github("hzambran/hydroTSM");install_github("hzambran/hydroGOF")
-})
+  })
 
